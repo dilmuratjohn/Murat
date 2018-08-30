799f5fc3e35f31ef414007a413de0a9e25a2ad3f
Dilmuratjohn <duolikun@ihandy.com>
2018-08-30 20:08:26 +0800

    arrow

--- src/Application.cpp
+++ src/Application.cpp
@@ -21,13 +21,23 @@ const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.7f, 0.2f, 1.0f);\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
     "}\n\0";
 
 const float position[] = {
-     0.0f, -0.5f,  // bottom center
-     0.5f,  0.5f,  // top right
-    -0.5f,  0.5f   // top left
+     0.00f,  0.50f,
+    -0.50f,  0.00f,
+     0.50f,  0.00f,
+    -0.25f,  0.00f,
+    -0.25f, -0.25f,
+     0.25f,  0.00f,
+     0.25f, -0.25f
+};
+
+const unsigned int indices[] = {
+    0, 1, 2,
+    3, 4, 5,
+    4, 5, 6
 };
 
 int main()
@@ -111,17 +121,23 @@ int main()
     glDeleteShader(fs);
 
     //vertex 
-    unsigned int va; 
-    unsigned int vb;
+    unsigned int va, vb, ib;
+
     glGenVertexArrays(1, &va);
     glGenBuffers(1, &vb);
+    glGenBuffers(1, &ib);
     glBindVertexArray(va);
 
     glBindBuffer(GL_ARRAY_BUFFER, vb);
     glBufferData(GL_ARRAY_BUFFER, sizeof(position), position, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ib);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -130,7 +146,8 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
